8436838cce0e0f4017c894d8555555e9527cac17
nikmakra <nikmakra0@gmail.com>
2023-05-02 17:02:33 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const KEY = '61cfb4d0-fada-49fb-a583-8d14265b487f';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
